31a1641557541c48b9260abe16179fdc3e3942ee
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-02-17 23:38:34 +0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -56,6 +56,7 @@ cp -rf /persist-arch /home/"${usrname}"
 /usr/bin/runuser -u ${usrname} -- /persist-arch/yay.sh
 /usr/bin/runuser -u ${usrname} -- flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 echo 32 > /sys/block/sdb/queue/iosched/fifo_batch
+systemctl enable --now NetworkManager
 #grub install
 case $boot in
 1)
