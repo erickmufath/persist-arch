fc4f24972621f9a567b407af98d84e819879b38b
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-02-17 23:37:49 +0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -55,6 +55,7 @@ cp -rf /persist-arch /home/"${usrname}"
 
 /usr/bin/runuser -u ${usrname} -- /persist-arch/yay.sh
 /usr/bin/runuser -u ${usrname} -- flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+echo 32 > /sys/block/sdb/queue/iosched/fifo_batch
 #grub install
 case $boot in
 1)
