00c9f7a7000e5a841e59fe26c1e03d33222e8891
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-02-22 19:03:46 +0700

    Update after-install.sh

--- after-install.sh
+++ after-install.sh
@@ -1,5 +1,13 @@
 source /persist-arch/install.conf
 ./persist-arch/yay.sh
+case $display in
+1)
+sudo pacman -Sy --noconfirm --needed - < persist-arch/_pulseaudio.txt
+;;
+2)
+yay -Sy --noconfirm --needed - < persist-arch/_pipewire.txt
+;;
+esac
 yay -Sy --nocofirm --needed - < /persist-arch/_aur.txt
 sudo systemctl enable ananicy
 sudo systemctl enable irqbalance
